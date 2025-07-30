cbccae585cf43cce019a9e685de00118c6e47263
grorp <grorp@posteo.de>
2025-07-30 18:53:40 +0200

    Making a window with winit
    According to https://docs.rs/winit/latest/winit/
    Note: Doesn't appear on Wayland, since nothing is presented

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,41 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io::{self, Write};
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Window, WindowId};
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
 
-    let correct = rand::rng().random_range(1..=100);
-
-    loop {
-        print!("Guess a number between 1 and 100: ");
-        io::stdout().flush().unwrap();
-
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        let Ok(guess) = input.trim().parse::<i32>() else {
-            println!("Invalid.");
-            continue;
-        };
-
-        match correct.cmp(&guess) {
-            Ordering::Greater => println!("Guess higher."),
-            Ordering::Less => println!("Guess lower."),
-            Ordering::Equal => {
-                println!("Correct!");
-                break;
-            }
-        }
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let attr = Window::default_attributes().with_title("Cubetonic");
+        self.window = Some(event_loop.create_window(attr).unwrap());
     }
 
-    println!("Goodbye, world.");
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
+    }
+}
+
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app).unwrap();
 }
