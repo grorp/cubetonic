6913f545c791c6e9d10225d1ced58eab4f4a0929
grorp <grorp@posteo.de>
2025-07-30 19:37:21 +0200

    Some wgpu init

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,60 @@
+use std::sync::Arc;
+
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
 use winit::window::{Window, WindowId};
 
+struct State {
+    window: Arc<Window>,
+    surface: wgpu::Surface<'static>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+}
+
+impl State {
+    async fn new(window: Arc<Window>) -> State {
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor::default());
+
+        let surface = instance.create_surface(window.clone()).unwrap();
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                compatible_surface: Some(&surface),
+                ..wgpu::RequestAdapterOptions::default()
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor::default())
+            .await
+            .unwrap();
+
+        State {
+            window,
+            surface,
+            device,
+            queue,
+        }
+    }
+}
+
 #[derive(Default)]
 struct App {
-    window: Option<Window>,
+    state: Option<State>,
 }
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         let attr = Window::default_attributes().with_title("Cubetonic");
-        self.window = Some(event_loop.create_window(attr).unwrap());
+        let window = Arc::new(event_loop.create_window(attr).unwrap());
+
+        let state = pollster::block_on(State::new(window.clone()));
+        self.state = Some(state);
+
+        window.request_redraw();
     }
 
     fn window_event(
@@ -20,12 +63,14 @@ impl ApplicationHandler for App {
         _window_id: WindowId,
         event: WindowEvent,
     ) {
+        let state = self.state.as_mut().unwrap();
+
         match event {
             WindowEvent::CloseRequested => {
                 event_loop.exit();
             }
             WindowEvent::RedrawRequested => {
-                self.window.as_ref().unwrap().request_redraw();
+                state.window.request_redraw();
             }
             _ => (),
         }
